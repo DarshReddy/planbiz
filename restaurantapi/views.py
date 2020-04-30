b83e40281de3868f85c1aad99b390b0ee78820e7
DarshReddy <darshreddy14@gmail.com>
2020-04-30 23:48:50 +0530

    blind try

--- restaurantapi/views.py
+++ restaurantapi/views.py
@@ -190,16 +190,17 @@ class DateMatchViewSet(viewsets.ModelViewSet):
 
     date = DateMatch.objects.get(id=pk)
     date.dateaccepted = True
+
     try:
       visitguy = HasVisited.objects.get(user=date.guy,restaurant=date.restaurant)
     except:
       visitguy = HasVisited.objects.create(user=date.guy,restaurant=date.restaurant)
+      visitguy.save()
     try:
       visitgirl = HasVisited.objects.get(user=date.girl,restaurant=date.restaurant)
     except:
       visitgirl = HasVisited.objects.create(user=date.guy,restaurant=date.restaurant)
-    visitguy.save()
-    visitgirl.save()
+      visitgirl.save()
     if request.user.is_female:
       date.girl = request.user
       fcm = FCMDevice.objects.get(user=date.guy)
